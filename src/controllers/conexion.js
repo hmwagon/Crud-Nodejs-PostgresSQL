e86d9064952f4f766a2f1df86ece46826647d25f
Miguel De jesus <the.mdj.miguel.23@hotmail.com>
2020-09-21 20:36:47 -0400

    Update Conexion PostGresSQL

--- src/controllers/conexion.js
+++ src/controllers/conexion.js
@@ -1,11 +1,11 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'miguel',
-    password: 'sami2345',
-    database: 'nodeapi',
-    port: '5432'
+    host: 'db-postgresql-fra1-31832-do-user-7159621-0.b.db.ondigitalocean.com',
+    user: 'doadmin',
+    password: 'zf32vd2hp5u675nx',
+    database: 'api',
+    port: '25060'
 });
 
 module.exports = pool;
